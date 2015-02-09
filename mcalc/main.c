3916a7dad73587933da920c365e190d8151b995a
Scott English <bonnieline@Bonnie-Lines-MacBook-Air-4.local>
2015-02-08 20:58:44 -0600

    Initial Commit

---
+++ mcalc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mcalc
+//
+//  Created by Scott English on 01/6/2012.
+//  Copyright (c) 2015 Scott English. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
